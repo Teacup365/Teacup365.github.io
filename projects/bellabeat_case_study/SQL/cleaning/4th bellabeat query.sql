f4e63a2df8c8e2e0287a1a716d4421ea3042d9f6
Tianee C. <tianeecarnakie@gmail.com>
2026-06-05 20:15:03 -0400

    Delete projects/bellabeat_case_study/SQL/cleaning/4th bellabeat query.sql

--- projects/bellabeat_case_study/SQL/cleaning/4th bellabeat query.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE TABLE `avid-lacing-483619-b2.bellabeat.sleepydayclean` AS
-SELECT
-Id,
-DATE(SleepDay) AS SleepDate,
-TotalSleepRecords,
-TotalMinutesAsleep,
-TotalTimeInBed
-FROM `avid-lacing-483619-b2.bellabeat.sleepyday`;
